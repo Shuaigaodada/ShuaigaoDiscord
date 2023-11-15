874c253325bf08dab7377dfab7c5eb895991885d
Laogao <2983536011@qq.com>
2023-11-14 21:20:37 -0600

    通过assets来管理src资源

---
+++ ShuaigaoDiscord/assets.py
@@ -0,0 +1,38 @@
+import os
+import pathlib
+
+root    = os.path.abspath(__file__)
+root    = os.path.dirname(root)
+src     = os.path.join(root, "src")
+server  = os.path.join(src, "servers")
+
+class Server:
+    def __init__(self, id: str):
+        self.id = id
+        self.user: User = self._assign_user
+    
+    def _assign_user(self, id: str):
+        self.user = User(id, self.id)
+
+class User:
+    def __init__(self, id: str, svid: str):
+        self.id = id
+        self.server_id = svid
+        self.path = os.path.join(server, self.server_id, self.id)
+    
+    def check(self, name: str) -> bool:
+        path = os.path.join(self.path, name)
+        return os.path.exists(path)
+
+    def create(self, name: str):
+        path = os.path.join(self.path, name)
+        os.makedirs(path, exist_ok=True)
+        return path
+
+    @property
+    def playlist(self):
+        os.makedirs(self.path, exist_ok=True)
+        playlists = os.listdir(self.path)
+        return playlists
+
+
