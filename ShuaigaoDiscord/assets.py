8dc6c6457c93b6204e8d245495f61ab88448f1dd
Laogao <2983536011@qq.com>
2023-11-15 23:01:37 -0600

    重构文件路径获取方式

--- ShuaigaoDiscord/assets.py
+++ ShuaigaoDiscord/assets.py
@@ -1,41 +1,93 @@
 import os
 import pathlib
+from typing import Any
 
-root    = os.path.abspath(__file__)
-root    = os.path.dirname(root)
-src     = os.path.join(root, "src")
-server  = os.path.join(src, "servers")
+_root    = os.path.abspath(__file__)
+_root    = os.path.dirname(_root)
+_src     = os.path.join(_root, "src")
+_server  = os.path.join(_src, "servers")
 
-class Server:
-    def __init__(self, id: str):
+# class Server:
+#     def __init__(self, id: str):
+#         self.id = id
+#         self.user: User = self._assign_user
+    
+#     def _assign_user(self, id: str):
+#         self.user = User(id, self.id)
+
+# class User:
+#     def __init__(self, id: str, svid: str):
+#         self.id = id
+#         self.server_id = svid
+#         self.path = os.path.join(_server, self.server_id, self.id)
+    
+#     def check(self, name: str) -> bool:
+#         path = os.path.join(self.path, name)
+#         return os.path.exists(path)
+
+#     def create(self, name: str):
+#         path = os.path.join(self.path, name)
+#         os.makedirs(path, exist_ok=True)
+#         return path
+    
+#     def lists(self, name: str):
+#         return os.listdir(os.path.join(self.path, name))
+
+#     def file(self, name: str):
+#         return os.path.join(self.path, name)
+
+#     @property
+#     def playlist(self):
+#         os.makedirs(self.path, exist_ok=True)
+#         playlists = os.listdir(self.path)
+#         return [file.split(".")[0] for file in playlists]
+
+class _AssetsFile:
+    def lists(self, suffix: bool = True):
+        if self._auto:
+            os.makedirs(self._path, exist_ok=True)
+        return os.listdir(self._path) if suffix else [f.split(".")[0] for f in os.listdir(self._path)]
+    
+    def join(self, name):
+        if self._auto:
+            os.makedirs(self._path, exist_ok=True)
+        return os.path.join(self._path, name)
+
+
+class User(_AssetsFile):
+    def __init__(self, id: str, _sid: str, _path: str, _auto: bool):
         self.id = id
-        self.user: User = self._assign_user
+        self._sid = _sid
+        self._path = _path
+        self._auto = _auto
     
-    def _assign_user(self, id: str):
-        self.user = User(id, self.id)
+    
 
-class User:
-    def __init__(self, id: str, svid: str):
+class Server(_AssetsFile):
+    def __init__(self, id: str, _path: str, _auto: bool):
         self.id = id
-        self.server_id = svid
-        self.path = os.path.join(server, self.server_id, self.id)
-    
-    def check(self, name: str) -> bool:
-        path = os.path.join(self.path, name)
-        return os.path.exists(path)
+        self._path = _path
+        self._auto = _auto
 
-    def create(self, name: str):
-        path = os.path.join(self.path, name)
-        os.makedirs(path, exist_ok=True)
-        return path
-    
-    def lists(self, name: str):
-        return os.listdir(os.path.join(self.path, name))
+    def user(self, id: str):
+        return User(id, self.id, os.path.join(self._path, id), self._auto)
 
-    @property
-    def playlist(self):
-        os.makedirs(self.path, exist_ok=True)
-        playlists = os.listdir(self.path)
-        return playlists
+    
+
+class Assets:
+    def __init__(self, path: str = _root, __auto: bool = True):
+        self._cph = path
+        self._auto = __auto
+    
+    def server(self, id: str):
+        return Server(id, os.path.join(self._cph, "src", "servers", str(id)), self._auto)
+
+    def __getattr__(self, attr: str):
+        return Assets(os.path.join(self._cph, attr), self._auto)
+
+    def path(self):
+        return self._cph
+    def file(self, name: str):
+        return os.path.join(self._cph, name)
 
 
