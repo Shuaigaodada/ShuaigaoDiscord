ab7bd5c1e4bc0f647bd237220feeb69775cad329
Laogao <2983536011@qq.com>
2023-11-15 23:02:26 -0600

    新增select_list的id

--- ShuaigaoDiscord/command.py
+++ ShuaigaoDiscord/command.py
@@ -1,3 +1,9 @@
 add_playlist    = "add_playlist"
 createlist      = "createlist"
+select_list     = "select_list"
+download_list   = "download_list"
+list_url        = "playlist_url"
+list_name       = "playlist_name"
+vip_username    = "vip_username"
+vip_password    = "vip_password"
 
